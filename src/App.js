951edc0e1780aa2b2e897c412197c834a72051b4
William Moore <w.moore@dundee.ac.uk>
2019-06-17 11:15:18 +0100

    Load Projects and Screens & sort

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios';
 
 function App() {
 
@@ -8,24 +7,62 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        'http://idr.openmicroscopy.org/api/v0/m/screens/',
-      );
+      let base_url = "http://idr.openmicroscopy.org/";
+      // Load Projects AND Screens, sort them and render...
+      Promise.all([
+        fetch(base_url + "api/v0/m/projects/?childCount=true"),
+        fetch(base_url + "api/v0/m/screens/?childCount=true"),
+      ]).then(responses =>
+          Promise.all(responses.map(res => res.json()))
+      ).then(([projects, screens]) => {
+          let studies = projects.data;
+          studies = studies.concat(screens.data);
+    
+          // ignore empty studies with no images
+          studies = studies.filter(study => study['omero:childCount'] > 0);
+    
+          // sort by name, reverse
+          studies.sort(function(a, b) {
+            var nameA = a.Name.toUpperCase();
+            var nameB = b.Name.toUpperCase();
+            if (nameA < nameB) {
+              return 1;
+            }
+            if (nameA > nameB) {
+              return -1;
+            }
+            // names must be equal
+            return 0;
+          });
 
-      setData({studies: result.data.data});
+          // Add 'id', 'type' ("project"), studyId ("screen-1") to each
+          studies = studies.map(study => {
+            study.id = study['@id'];
+            study.type = study['@type'].split('#')[1].toLowerCase();
+            study.objId = `${ study.id }-${ study.type }`;
+            return study;
+          });
+
+          setData({studies});
+      }).catch((err) => {
+        console.error(err);
+      });
     };
 
     fetchData();
   }, []);
 
   return (
-    <ul>
-      {data.studies.map(study => (
-        <li key={study.id}>
-          {study.Name}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1>Welcome to IDR</h1>
+      <ul>
+        {data.studies.map(study => (
+          <li key={study.objId}>
+            {study.Name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
