46a827ceb7a42e06d6e7e963fd986ab1004d7bf8
William Moore <w.moore@dundee.ac.uk>
2019-06-17 10:53:05 +0100

    Init - load screens and render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState({ studies: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://idr.openmicroscopy.org/api/v0/m/screens/',
+      );
+
+      setData({studies: result.data.data});
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {data.studies.map(study => (
+        <li key={study.id}>
+          {study.Name}
+        </li>
+      ))}
+    </ul>
   );
 }
 
