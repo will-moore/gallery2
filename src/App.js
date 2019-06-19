52c808de3f0e46247a24c5641567a0b5c3055db1
William Moore <w.moore@dundee.ac.uk>
2019-06-19 10:04:58 +0100

    Move categories loading and list into Categories.js

--- src/App.js
+++ src/App.js
@@ -5,38 +5,9 @@ import './css/openmicroscopy.css';
 import './css/idr.css';
 import './css/studies.css';
 import { ReactComponent as Logo } from './logo-idr.svg';
-import Category from './Category';
-import { fetchStudies,
-         loadStudiesMapAnnotations,
-         loadStudiesThumbnails } from './model/fetchData';
+import Categories from './Categories';
 
 function App() {
-
-  const [data, setData] = useState({ studies: [] });
-
-  const categories = [
-    {"label": "Most Recent", "index": 0, "query": "LAST10:date"},
-    {"label": "Time-lapse imaging", "index": 1, "query": "Study Type:time OR Study Type:5D OR Study Type:3D-tracking"},
-    {"label": "Light sheet fluorescence microscopy", "index": 2, "query": "Study Type:light sheet"},
-    {"label": "Protein localization studies", "index": 3, "query": "Study Type:protein localization"},
-    {"label": "Digital pathology imaging", "index": 4, "query":"Study Type:histology"},
-    {"label": "Yeast studies", "index": 5, "query": "Organism: Saccharomyces cerevisiae OR Organism:Schizosaccharomyces pombe"},
-    {"label": "High-content screening (human)", "index": 6, "query": "Organism:Homo sapiens AND Study Type:high content screen"}
-  ];
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-
-      // Load studies, then load map annotations for them
-      let studies = await fetchStudies();
-      studies = await loadStudiesMapAnnotations(studies);
-      studies = await loadStudiesThumbnails(studies);
-      setData({studies});
-    };
-
-    fetchData();
-  }, []);
 
   let hrStyle = {
     height:0, margin: '8px'
@@ -74,14 +45,7 @@ function App() {
         </p>
       </div>
 
-      <div id="studies" className="row horizontal">
-        {categories.map(category => (
-          <Category
-            key={category.label}
-            data={category}
-            studies={data.studies} />
-        ))}
-      </div>
+      <Categories />
     </div>
   );
 }
---
+++ src/Categories.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Category from './Category';
+import { fetchStudies,
+         loadStudiesMapAnnotations,
+         loadStudiesThumbnails } from './model/fetchData';
+
+function Categories() {
+
+  const [data, setData] = useState({ studies: [] });
+
+  const categories = [
+    {"label": "Most Recent", "index": 0, "query": "LAST10:date"},
+    {"label": "Time-lapse imaging", "index": 1, "query": "Study Type:time OR Study Type:5D OR Study Type:3D-tracking"},
+    {"label": "Light sheet fluorescence microscopy", "index": 2, "query": "Study Type:light sheet"},
+    {"label": "Protein localization studies", "index": 3, "query": "Study Type:protein localization"},
+    {"label": "Digital pathology imaging", "index": 4, "query":"Study Type:histology"},
+    {"label": "Yeast studies", "index": 5, "query": "Organism: Saccharomyces cerevisiae OR Organism:Schizosaccharomyces pombe"},
+    {"label": "High-content screening (human)", "index": 6, "query": "Organism:Homo sapiens AND Study Type:high content screen"}
+  ];
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      // Load studies, then load map annotations for them
+      let studies = await fetchStudies();
+      studies = await loadStudiesMapAnnotations(studies);
+      studies = await loadStudiesThumbnails(studies);
+      setData({studies});
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div id="studies" className="row horizontal">
+    {categories.map(category => (
+        <Category
+        key={category.label}
+        data={category}
+        studies={data.studies} />
+    ))}
+    </div>
+  );
+}
+
+export default Categories;
