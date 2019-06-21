55aec394eed77099191b28db1f3254299f5b26ef
William Moore <w.moore@dundee.ac.uk>
2019-06-21 16:44:41 +0100

    Fix links and study title in CategoryStudy.js

--- src/CategoryStudy.js
+++ src/CategoryStudy.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BASE_URL } from './model/fetchData';
 
 function CategoryStudy({study}) {
 
@@ -11,15 +12,12 @@ function CategoryStudy({study}) {
     <div key={study.objId} className="row study">
       <div>{study.id}</div>
       <div className="studyImage" style={studyStyle}>
-        <a target="_blank" href="/webclient/?show=project-801">
+        <a target="_blank" rel="noopener noreferrer" href={`${ BASE_URL }webclient/?show=${ study.objId }`}>
           <div>
             <div className="studyText">
               <p title="We provide a user-friendly 3D editing tool to rapidly correct segmentation errors.  We test our method alongside other previously-published user-friendly methods, with focus on mid-gestation embryos, 3D cultures of pluripotent stem cells and pluripotent stem cell-derived neural rosettes.">
-                NesSys: a novel method for accurate nuclear segmentation in 3D
+                {study.title}
               </p>
-            </div>
-            <div className="studyAuthors">
-              Guillaume Blin, Daina Sadurska, Rosa Portero Migueles, Naiming Chen, Julia A. Watson, Sally Lowell
             </div>
           </div>
         </a>
